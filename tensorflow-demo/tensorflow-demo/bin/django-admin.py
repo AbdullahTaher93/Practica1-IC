cbfaa55ee8b4a962668251728beb48a5d1c16ad4
AbdullahTaher93 <abdullahtaher@correo.ugr.es>
2019-11-24 14:26:24 +0100

    add python

---
+++ tensorflow-demo/tensorflow-demo/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/abdullah/Documents/Practica1-IC/tensorflow-demo/tensorflow-demo/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
